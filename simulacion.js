41ad197b3a267e09c6c9a052d88ffec21064203f
Nicolas <nikoalegre@hotmail.com>
2020-06-24 18:20:20 -0300

    Funciona todo. Falta corregir que se actualice cada vez que se apreta un click.

--- simulacion.js
+++ simulacion.js
@@ -5,6 +5,9 @@ var tv = [];//CREO UN ARREGLO DONDE VOY A IR CARGANDO LAS VENTAS PARA CADA MES (
 var cu = [];//CREO UN ARREGLO DONDE VOY A IR CARGANDO EL COSTO POR UNIDAD DE CADA MES
 var ct = [];//CREO UN ARREGLO DONDE VOY A IR CARGANDO LOS COSTOS TOTALES DE CADA MES
 var cantprod = []; //CREO UN ARREGLO PARA ALMACENAR CUANTOS PROD PRODUCIDOS HAY PARA CADA MES
+var demnosat = []; //CREO UN ARREGLO PARA ALMACENAR LA DEMANDA NO SATISFECHA DE CADA MES
+var costOp = []; //CREO UN ARREGLO PARA ALMACENAR EL COSTO DE OPORTUNIDAD DE CADA MES
+var ganancia = []; //EN ESTE ARREGLO VOY ALMACENANDO LA GANANCIA DE CADA MES
 
 // A PARTIR DE ACA PARA ABAJO VAN TODAS LAS FUNCIONES
 function random(min,max) { //DEVUELVE UN RANDOM ENTRE 3 Y 10
@@ -107,14 +110,65 @@ function obtenerCantProducida(){
 function obtenerCostoTotal(){// ANTES DE ESTA FUNCION TENEMOS QUE EJECUTAR LA DE OBTENERCANTPRODUCIDA O SINO NO PUEDE MULTIPLICAR
   for (i = 0; i < 12; i++) { //MULTIPLICO EL COSTO POR UNIDAD CON LA CANTIDAD DE PRODUCTOS PRODUCIDOS Y GUARDO EN EL ARREGLO ct
       ct.push(cu[i]*cantprod[i]);
-      console.log(cantprod[i]);
-      
     }
 
     for (i = 1; i < 13; i++) {//POR CADA MES LE VOY PASANDO EL VALOR QUE ESTA EN EL ARREGLO 
       id = "CostTot"+i;
       document.getElementById(id).innerHTML = ct[i-1]; //Hago i-1 porque este ciclo va de 1 a 12 y el arreglo de 0 a 11
     }
+}
+
+function obtenerDemandaNoSatis(){
+  for (i = 0; i < 12; i++) { 
+    if (cv[i]>cantprod[i]) {//SI CANT UNID VENDIDAS ES MAYOR A CANT UNIDADES PRODUCIDAS QUIERE DECIR QUE TENEMOS DEMANDA NO SATISFECHA
+      demnosat.push(cv[i]-cantprod[i]); //Agrego a demnosat cuantas unidades son las que no pude satisfacer
+    } else {
+      demnosat.push(0); //Agrego a demnosat un cero simplemente porque no tengo demanda no satisfechas
+    } 
+  }
+
+    for (i = 1; i < 13; i++) {//POR CADA MES LE VOY PASANDO EL VALOR QUE ESTA EN EL ARREGLO 
+      id = "DemNoSat"+i;
+      document.getElementById(id).innerHTML = demnosat[i-1]; //Hago i-1 porque este ciclo va de 1 a 12 y el arreglo de 0 a 11
+    }
+}
+
+function obtenerCostoOportunidad(){
+  for (i = 0; i < 12; i++) { 
+    costOp.push(demnosat[i]*1000); //MULTIPLICO LA DEM NO SATISFECHA POR 1000 Y AGREGO EN EL ARREGLO DE COSTO DE OPORTUNIDAD
+  }
+
+    for (i = 1; i < 13; i++) {//POR CADA MES LE VOY PASANDO EL VALOR QUE ESTA EN EL ARREGLO 
+      id = "CostOp"+i;
+      document.getElementById(id).innerHTML = costOp[i-1]; //Hago i-1 porque este ciclo va de 1 a 12 y el arreglo de 0 a 11
+    }
+}
+
+function obtenerGanancia(){
+  for (i = 0; i < 12; i++) { 
+    if (i<6){ //Con este condicional controlo de si es el primer semestre o el segundo para calcular el costo fijo
+              //Pregunta si es menor a 6 porque el arreglo comienza en 0, es decir, de 0 a 5 es el primer semestre
+      costofijo = 600000;
+    } else{
+      costofijo = 900000;
+    }
+    ganancia.push(tv[i]-ct[i]-costofijo-costOp[i]); //Calculo la ganancia con la formula del escenario
+  }
+
+    for (i = 1; i < 13; i++) {//POR CADA MES LE VOY PASANDO EL VALOR QUE ESTA EN EL ARREGLO 
+      id = "Ganancia"+i;
+      document.getElementById(id).innerHTML = ganancia[i-1]; //Hago i-1 porque este ciclo va de 1 a 12 y el arreglo de 0 a 11
+    }
+}
+
+function obtenerGananciaTotal(){
+  var gananciaTotal = 0; //Hay que inicializar a 0 porque es un acumulador
+  for (i = 0; i < 12; i++) { 
+    gananciaTotal = gananciaTotal + ganancia[i]; //Sumo en una variable las ganancias de cada mes para obtener el total
+    console.log(gananciaTotal);
+  }
+
+  document.getElementById("gananciaTotal").innerHTML = gananciaTotal;
 }
 
 
@@ -128,6 +182,11 @@ function ejecutarFunciones() { //ESTA FUNCION AGRUPA EL CONJUNTO DE FUNCIONES A 
     obtenerVentas();
     obtenerCostoUnidad();
     obtenerCantProducida();
-    obtenerCostoTotal();
+    obtenerCostoTotal();//Hay que tener cuidado de ejecutar esta despues de obtenerCantPruducida 
+                        //porque si no tenemos cuantas unidades producimos no podemos saber el costo total
+    obtenerDemandaNoSatis();
+    obtenerCostoOportunidad();
+    obtenerGanancia();
+    obtenerGananciaTotal();
 
 }
